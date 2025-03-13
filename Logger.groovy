6a0e7c90fa411a1f49327bf662d89a04e1eb9b8e
michelrodriguez862 <michel.rodriguez862@comunidadunir.net>
2025-03-13 17:41:00 +0100

    Create Logger.groovy

---
+++ Logger.groovy
@@ -0,0 +1,8 @@
+class Logger {
+    // Método que registrará los mensajes
+    def log(String message) {
+        // Para simplicidad, se muestra el mensaje en la consola.
+        // Se puede redirigir a un archivo si se necesita.
+        println "[LOG] " + message
+    }
+}
